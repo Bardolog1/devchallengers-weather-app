0f75dede5898a4f61e29b9aedb152b9ef36d8e6f
Libardo Lozano <lilozanoga@unal.edu.co>
2023-07-28 22:02:00 -0500

    Se elim fiche de test y sus dep, se crea hook useFetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import LeftViewer from './components/LeftViewer';
+import LeftViewer  from './components/LeftViewer';
+import useFetch  from './hooks/UseFetch';
 
 const AppCont = styled.div`
   text-align: center;
   background: #100E1D;
   min-height: 100vh;
   width: 100%;
+  
   font-size: calc(10px + 2vmin);
   color: white;
   position: relative;
@@ -21,9 +23,17 @@ const StyledLefViewer = styled(LeftViewer)`
 
 
 const App = ()=>{
+
+  const { data }= useFetch('https://countriesnow.space/api/v0.1/countries/population/cities');
   return (
     <AppCont>
       <StyledLefViewer/>
+      <div className="container">
+      <ul>
+        {data?data.map((item) => (
+          <li key={item.city}>{item.city}</li>)):null}
+      </ul>
+    </div>
     </AppCont>
   );
 }
---
+++ src/hooks/UseFetch.js
@@ -0,0 +1,18 @@
+import {useState, useEffect} from 'react';
+
+
+export function useFetch(url){
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            setData(data);
+        });
+    },[]);
+
+
+    return {data};
+}
